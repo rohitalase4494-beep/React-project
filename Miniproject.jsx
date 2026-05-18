b5ad777f9cce9db7158d8e2db51788be8ce1293d
rohitalase4494-beep <rohitalase108@gmail.com>
2026-05-18 22:46:37 +0530

    Add files via upload

---
+++ Miniproject.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function Miniproject(){
+    const colors=JSON.parse(localStorage.getItem('color'))
+    const[r,setR]=useState(colors&&colors.r?colors.r:0)
+    const[g,setG]=useState(colors&&colors.g?colors.g:0)
+    const[b,setB]=useState(colors&&colors.b?colors.b:0)
+
+    const save=()=>{
+        console.log("saved")
+        localStorage.setItem("color",JSON.stringify({r,g,b}))
+    }
+    return(
+        <div>
+            <h1>Color Mixer</h1>
+            <div style={{backgroundColor:'rgb('+r+','+g+','+b+')', height:200, width:200}}></div>
+            <label htmlFor="">Red</label>
+            <input type="range" value={r} onChange={(event)=>setR(event.target.value)} min={0} max={255}/>
+            <br></br>
+            <br></br>
+               <label htmlFor="">Green</label>
+            <input type="range" value={g} onChange={(event)=>setG(event.target.value)} min={0} max={255}/>
+            <br></br>
+            <br></br>
+               <label htmlFor="">Blue</label>
+            <input type="range" value={b} onChange={(event)=>setB(event.target.value)} min={0} max={255}/>
+            <br></br>
+            <br></br>
+            <button onClick={save}>save color combination</button>
+        </div>
+    )
+}
+
